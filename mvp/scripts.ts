c0a89aace2587948f85266a1fbddc8164140ecf9
oui <mohamed_elhani@outlook.fr>
2025-06-08 14:46:21 +0200

    Likes and date support + small css improvements

--- mvp/scripts.ts
+++ mvp/scripts.ts
@@ -7,8 +7,8 @@ interface messageData{
     username:string;
     content:string;
     like:number;
-    created:string;
-    updated:string;
+    created_at:string;
+    updated_at:string;
 
 
 }
@@ -19,38 +19,45 @@ interface messagesData{
 }
 const divcontent:HTMLTextAreaElement=document.getElementById("message") as HTMLTextAreaElement;
 const divusername:HTMLInputElement=document.getElementById("username") as HTMLInputElement;
-const container:HTMLUListElement = document.getElementById("commentsList") as HTMLUListElement
+const container:HTMLUListElement = document.getElementById("messages-list") as HTMLUListElement
 let submit:HTMLButtonElement= document.getElementById("submit") as HTMLButtonElement;
 
 async function displaymessageAPI():Promise<void> {
     const data:messagesData=await getmessageAPI();
     if (data != undefined) {
         for (let i = 0; i <=20; i++) {
-            const newmessage:HTMLDivElement= createmessage(data.data[i].username,data.data[i].content,data.data[i].created)
+            const newmessage:HTMLDivElement= createmessage(data.data[i].username,data.data[i].content,data.data[i].created_at,data.data[i].like)
             container.appendChild(newmessage);
 
-
+            
             
         }
     }
 }
-function createmessage(name:string,body:string,timestamp:string):HTMLDivElement {
+function createmessage(name:string,body:string,timestamp:string,like:number):HTMLDivElement {
     const message:HTMLDivElement = document.createElement("div");
     const namep:HTMLParagraphElement = document.createElement("p");
     const timestampspan:HTMLSpanElement = document.createElement("span");
     const bodyp:HTMLParagraphElement = document.createElement("p");
+    const likespan:HTMLSpanElement = document.createElement("span"); 
+    const metadiv:HTMLDivElement=document.createElement("div"); 
+    
 
     message.classList.add("message");
     namep.textContent = name;
     namep.classList.add("msg-name");
-    timestampspan.textContent = timestamp
+    timestampspan.textContent = format_date(timestamp)
     timestampspan.classList.add("msg-date");
     bodyp.classList.add("msg-body");
     bodyp.textContent= body;
-
-    namep.appendChild(timestampspan);
+    likespan.classList.add("msg-likes")
+    likespan.textContent="❤️   "+ String(like)
+    metadiv.classList.add("msg-meta")
+    metadiv.appendChild(likespan);
+    metadiv.appendChild(timestampspan);
     message.appendChild(namep);
     message.appendChild(bodyp);
+    message.appendChild(metadiv)
     
     return message;
 
@@ -65,7 +72,7 @@ async function getmessageAPI():Promise<messagesData> {
     }
     else {
         console.log("Error : "+res.status);
-        throw new Error("aie");
+        throw new Error("oops");
          
     }
 }
@@ -86,7 +93,46 @@ async function sendmessageAPI() {
         })
         
     })
-    console.log("aled");  
+}
+
+function pad(word:string,pad_size:number):string{
+    if(word.length>pad_size){
+        return word;
+    }
+    else{
+        let res : string = "";
+        for (let i = 0; i < pad_size-word.length; i++) {
+            res+="0";
+        }
+        res+=word;
+        return res;
+    }
+}
+
+function format_date(timestamp:string):string{
+
+    const date : Date = new Date(timestamp);
+    let formatted_date_time : string;
+    
+    let hours: number = date.getHours();
+    const minutes: number = date.getMinutes();
+    
+    if(hours<1){
+        formatted_date_time = `${minutes} ago`;
+    }
+    else{
+
+        const formattedTime: string = `${hours}:${pad(String(minutes),2)}`;
+        
+        const day: number = date.getDate();
+        const month: number = date.getMonth() + 1;
+        const year: number = date.getFullYear();
+        const formattedDate: string = `${pad(String(day),2)}/${pad(String(month),2)}/${year}`;
+
+        formatted_date_time = `${formattedDate} at ${formattedTime}`;
+    }
+
+    return formatted_date_time;
 }
 
 submit.addEventListener("click",async (e:Event)=>{
